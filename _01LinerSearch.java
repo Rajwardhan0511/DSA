3c7d295d4df03bff175d31328958be291071f383
Rajwardhan Shinde <arsshinde2151@gmail.com>
2024-11-13 23:51:46 +0530

    Second code of DSA

--- _01LinerSearch.java
+++ _01LinerSearch.java
@@ -1,18 +1,19 @@
 public class _01LinerSearch {
-    private static int linerSearch(int[] arr,int target){
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]==target)
+    private static int linerSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target)
                 return i;
         }
         return -1;
     }
-    public static void main(String[] args){
-        int arr[] = {3,2,6,4,7,8,3};
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 2, 6, 4, 7, 8, 3 };
         int target = 8;
         int result = linerSearch(arr, target);
-        if(result != -1){
-            System.out.println("Found at index: "+result);
-        }else{
+        if (result != -1) {
+            System.out.println("Found at index: " + result);
+        } else {
             System.out.println("Target not found");
         }
     }
---
+++ _02BinarySearch.java
@@ -0,0 +1,30 @@
+public class _02BinarySearch {
+    private static int BinarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 2, 6, 4, 7, 8, 3 };
+        int target = 8;
+        int result = BinarySearch(arr, target);
+        if (result != -1) {
+            System.out.println("Found at index: " + result);
+        } else {
+            System.out.println("Target not found");
+        }
+    }
+}
